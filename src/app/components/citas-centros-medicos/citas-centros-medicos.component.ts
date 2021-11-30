5cc7a5c5a663bbca9aee74e1a453bdd635385547
mmedel <matias.medel.18@gmail.com>
2021-11-30 10:43:16 -0300

    Componente ficha-clinica y citas-centros-medicos agregados

---
+++ src/app/components/citas-centros-medicos/citas-centros-medicos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-citas-centros-medicos',
+  templateUrl: './citas-centros-medicos.component.html',
+  styleUrls: ['./citas-centros-medicos.component.css']
+})
+export class CitasCentrosMedicosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -5,24 +5,34 @@ import { FormsModule } from '@angular/forms'
 import { CitasComponent } from './citas/citas.component';
 import { ConfirmacionCitaComponent } from './confirmacion-cita/confirmacion-cita.component';
 import { TablaResultadosComponent } from './tabla-resultados/tabla-resultados.component';
+import { FichaClinicaComponent } from './ficha-clinica/ficha-clinica.component';
+import { CitasCentrosMedicosComponent } from './citas-centros-medicos/citas-centros-medicos.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
   declarations: [
     CitasComponent,
     ConfirmacionCitaComponent,
-    TablaResultadosComponent
+    TablaResultadosComponent,
+    FichaClinicaComponent,
+    CitasCentrosMedicosComponent,
+    LoginComponent
 
 
   ],
-  exports: [
+
+  exports:[
     CitasComponent,
     ConfirmacionCitaComponent,
-    TablaResultadosComponent
-
+    TablaResultadosComponent,
+    FichaClinicaComponent,
+    CitasCentrosMedicosComponent,
+    LoginComponent
 
 
   ],
+  
   imports: [
     BrowserModule,
     CommonModule,
