2db144ff9dd2f6f2039aefdd976d8a30f341ff0c
dcarrasco <dcarrascocid@gmail.com>
2021-12-22 13:52:40 -0300

    ip deevelop

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,11 @@
 
 export const environment = {
   production: false,
-  base_url: 'http://localhost:3001/api'
+  // base_url: 'http://localhost:3001/api'
   // base_url: 'http://a2d7-201-188-63-168.ngrok.io/api'
+  base_url: 'http://186.107.135.158:3001/api'
+
+  
 };
 
 /*
