17ada34fd74eaaa226fb2b2d7d90756727eb50a4
dcarrasco <dcarrascocid@gmail.com>
2021-12-27 15:28:48 -0300

    configuracion ngrok

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  // base_url: 'http://a2d7-201-188-63-168.ngrok.io/api'
-  base_url: 'http://186.107.135.158:3001/api'
+  base_url: 'http://9921-186-107-135-158.ngrok.io/api'
+
 };
